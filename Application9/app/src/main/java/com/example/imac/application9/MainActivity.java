7fbdb63ba6d0a9385796ae7380b1b76b353a1629
eabichahine <elie.abichahine@gmail.com>
2015-09-30 06:22:08 -0700

    Learning Android - Commiting Application 9

---
+++ Application9/app/src/main/java/com/example/imac/application9/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.imac.application9;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        RelativeLayout main_view = (RelativeLayout)findViewById(R.id.main_view);
+
+        switch(item.getItemId())
+        {
+            case R.id.menu_red:
+            {
+                if(item.isChecked())
+                {
+                    item.setChecked(false);
+                }
+                else
+                {
+                    item.setChecked(true);
+                }
+                main_view.setBackgroundColor(Color.RED);
+            }
+            break;
+
+            case R.id.menu_green:
+            {
+                if(item.isChecked())
+                {
+                    item.setChecked(false);
+                }
+                else
+                {
+                    item.setChecked(true);
+                }
+                main_view.setBackgroundColor(Color.GREEN);
+            }
+            break;
+
+            case R.id.menu_yellow:
+            {
+                if(item.isChecked())
+                {
+                    item.setChecked(false);
+                }
+                else
+                {
+                    item.setChecked(true);
+                }
+                main_view.setBackgroundColor(Color.YELLOW);
+            }
+            break;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+        return true;
+    }
+}
